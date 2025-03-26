692a6b0f5852a7a3d68d584d92fce6b465928f58
gjtjrl303 <gjtjrl303@naver.com>
2025-03-26 23:30:29 +0900

    feat: main 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,10 @@
 import controller.LottoController;
-import domain.LottoMachine;
-import domain.numberGenerator.LottoNumberGenerator;
-import domain.numberGenerator.NumberGenerator;
-import domain.LottoShop;
-import view.LottoView;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        NumberGenerator numberGenerator = new LottoNumberGenerator();
-        LottoMachine lottoMachine = new LottoMachine(numberGenerator);
-        LottoShop lottoShop = new LottoShop(lottoMachine);
-
-        LottoController lottoController = new LottoController(lottoShop, new LottoView());
+        LottoController lottoController = new LottoController();
 
         lottoController.inputMoney();
         lottoController.printLottoCount();
