1936e1ddb319d48a3fc222a19aa864b02995439d
gjtjrl303 <gjtjrl303@naver.com>
2025-03-27 08:49:17 +0900

    feat: pirzeMoney 객체 구현

--- src/main/java/domain/PrizeMoney.java
+++ src/main/java/domain/PrizeMoney.java
@@ -5,7 +5,6 @@ public class PrizeMoney {
     private final double amount;
 
     public PrizeMoney(double amount) {
-        validate(amount);
         this.amount = amount;
     }
 
@@ -19,12 +18,6 @@ public class PrizeMoney {
 
     public double getAmount() {
         return amount;
-    }
-
-    private void validate(double amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("상금은 0보다 작을 수 없습니다");
-        }
     }
 
 }
