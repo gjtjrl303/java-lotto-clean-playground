899bebee8628b757e3f3cb168137ec8e0919abcc
gjtjrl303 <gjtjrl303@naver.com>
2025-03-26 23:30:48 +0900

    feat: 뷰 구현

---
+++ src/main/java/view/LottoView.java
@@ -0,0 +1,63 @@
+package view;
+
+import domain.Lotto;
+import domain.LottoResult;
+import enums.LottoRank;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class LottoView {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public int inputMoneyView() {
+        System.out.println("구입금액을 입력해 주세요");
+        int money = scanner.nextInt();
+        return money;
+    }
+
+    public void printLottosView(List<Lotto> lottos) {
+        for (int i = 0; i < lottos.size(); i++) {
+            printLotto(lottos.get(i));
+        }
+    }
+
+    public String inputWinningNumbersView() {
+        scanner.nextLine();
+        System.out.println();
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+         return scanner.nextLine();
+
+    }
+
+    public void printLotto(Lotto lotto) {
+        List<Integer> lottoNumbers = lotto.getLottoNumbers();
+        System.out.println(lottoNumbers);
+
+    }
+
+    public void printLottoCount(Integer lottoCount) {
+        System.out.println();
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public void printResultByRanks(Map<LottoRank, Integer> resultByRank) {
+        System.out.println("당첨 통계");
+        System.out.println("-------");
+        for (LottoRank lottoRank : resultByRank.keySet()) {
+            printSingleRankResult(resultByRank, lottoRank);
+        }
+    }
+
+    private void printSingleRankResult(Map<LottoRank, Integer> resultByRank, LottoRank lottoRank) {
+        if (!(lottoRank == LottoRank.NO_MATCH)) {
+            System.out.println(lottoRank.getMatchCount() + "개 일치 (" + lottoRank.getPrize() + ")- " + resultByRank.get(lottoRank));
+        }
+    }
+
+    public void printProfitRate(Double profitRate) {
+        System.out.println("총 수익률은 " + profitRate + "입니다.");
+    }
+}
