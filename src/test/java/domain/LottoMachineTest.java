57e5c19e599dce0abb16a0a9eb9fc5f801ebd907
gjtjrl303 <gjtjrl303@naver.com>
2025-03-27 09:49:26 +0900

    feat: fakenumberGenerator를 이용한 테스트 코드 구현

--- src/test/java/domain/LottoMachineTest.java
+++ src/test/java/domain/LottoMachineTest.java
@@ -1,42 +1,44 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class LottoMachineTest {
 
-    LottoMachine lottoMachine = new LottoMachine();
+    LottoMachine lottoMachine;
 
-    @Test
-    @DisplayName("로또머신에_돈이_들어가면_올바른_로또수를_계산해야한다")
-    void 로또머신에_돈이_들어가면_올바른_로또수를_계산해야한다() {
-
-        //given
-        lottoMachine.inputMoney(new Money(14000));
-
-        //when
-        int lottoCount = lottoMachine.getLottoCount();
-
-        //then
-        assertThat(lottoCount).isEqualTo(14);
+    @BeforeEach
+    void beforeEach() {
+        List<List<Integer>> fixedNumbersList = List.of(
+                new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)),
+                new ArrayList<>(List.of(2, 3, 4, 5, 6, 7)),
+                new ArrayList<>(List.of(3, 4, 5, 6, 7, 8)),
+                new ArrayList<>(List.of(4, 5, 6, 7, 8, 9)),
+                new ArrayList<>(List.of(5, 6, 7, 8, 9, 10)),
+                new ArrayList<>(List.of(7, 8, 9, 10, 11, 12))
+        );
+        lottoMachine = new LottoMachine(new FakeNumberGenerator(fixedNumbersList));
     }
 
     @Test
-    @DisplayName("로또수만큼_로또를_생성해야한다")
-    void 로또수만큼_로또를_생성해야한다() {
+    @DisplayName("값에_맞는_로또수를_생성해야한다")
+    void 값에_맞는_로또수를_생성해야한다() {
 
         //given
-        lottoMachine.inputMoney(new Money(14000));
+        lottoMachine.inputMoney(new Money(5000));
 
         //when
         int lottoCount = lottoMachine.getLottoCount();
         lottoMachine.generateLottos();
 
         //then
-        assertThat(lottoMachine.getLottos().size()).isEqualTo(14);
+        assertThat(lottoMachine.getLottos().size()).isEqualTo(5);
     }
 
     @Test
--- src/test/java/domain/LottoResultTest.java
+++ src/test/java/domain/LottoResultTest.java
@@ -2,7 +2,6 @@ package domain;
 
 
 import enums.LottoRank;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
