eb628f1d09317bc17e2c1806ac19274b17c37c88
gjtjrl303 <gjtjrl303@naver.com>
2025-03-30 09:22:02 +0900

    refactor: LottoNumber의 필드명 value에서 number로 변경

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -5,19 +5,19 @@ public class LottoNumber  {
     private static final int MIN = 1;
     private static final int MAX = 45;
 
-    private final int value;
+    private final int number;
 
-    public LottoNumber(int value) {
-        validate(value);
-        this.value = value;
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
     }
 
-    public int getValue() {
-        return value;
+    public int getNumber() {
+        return number;
     }
 
-    private void validate(int value) {
-        if (value < MIN || value > MAX) {
+    private void validate(int number) {
+        if (number < MIN || number > MAX) {
             throw new IllegalArgumentException("로또 번호는 " + MIN + " 이상 " + MAX + " 이하의 숫자여야 합니다.");
         }
     }
