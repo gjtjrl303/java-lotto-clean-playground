cd38d2de04eff496166fc8daf5f211414b314791
gjtjrl303 <gjtjrl303@naver.com>
2025-03-30 09:22:47 +0900

    feat: equals, hashCode, toString, compareTo 구현

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -1,6 +1,8 @@
 package domain;
 
-public class LottoNumber  {
+import java.util.Objects;
+
+public class LottoNumber implements Comparable<LottoNumber> {
 
     private static final int MIN = 1;
     private static final int MAX = 45;
@@ -20,5 +22,27 @@ public class LottoNumber  {
         if (number < MIN || number > MAX) {
             throw new IllegalArgumentException("로또 번호는 " + MIN + " 이상 " + MAX + " 이하의 숫자여야 합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        LottoNumber that = (LottoNumber) object;
+        return number == that.number;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
+
+    @Override
+    public int compareTo(LottoNumber other) {
+        return Integer.compare(this.number, other.number);
     }
 }
