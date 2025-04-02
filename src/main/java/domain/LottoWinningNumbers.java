01c45e55c4fa4714db8dc37918244bbf9150b4d5
gjtjrl303 <gjtjrl303@naver.com>
2025-04-02 20:21:52 +0900

    feat: 로또 보너스 번호 로직 구현

--- src/main/java/domain/LottoWinningNumbers.java
+++ src/main/java/domain/LottoWinningNumbers.java
@@ -5,14 +5,31 @@ import java.util.List;
 public class LottoWinningNumbers {
 
     private final Lotto winningNumbers;
+    private final LottoNumber bonusNumber;
 
-    public LottoWinningNumbers(List<Integer> numbers) {
+    private LottoWinningNumbers(List<Integer> numbers, int bonusNumber) {
         winningNumbers = Lotto.from(numbers);
+        this.bonusNumber = LottoNumber.from(bonusNumber);
+        validate();
+    }
+
+    public static LottoWinningNumbers from(List<Integer> numbers, int bonusNumbers) {
+        return new LottoWinningNumbers(numbers, bonusNumbers);
     }
 
     public int matchCount(Lotto lotto) {
         return (int) lotto.getLottoNumbers().stream()
                 .filter(number -> winningNumbers.getLottoNumbers().contains(number))
                 .count();
+    }
+
+    public boolean bonusMatch(Lotto lotto) {
+        return lotto.getLottoNumbers().contains(bonusNumber);
+    }
+
+    private void validate() {
+        if (winningNumbers.getLottoNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
     }
 }
