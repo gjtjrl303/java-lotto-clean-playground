99991f810ea41fcd678da0ad4b2d76903417e010
gjtjrl303 <gjtjrl303@naver.com>
2025-04-02 20:26:41 +0900

    feat: 보너스 번호, 수동 번호 입출력 구현

--- src/main/java/view/LottoView.java
+++ src/main/java/view/LottoView.java
@@ -1,22 +1,26 @@
 package view;
 
 import domain.Lotto;
+import domain.LottoCount;
 import domain.LottoNumber;
+import domain.LottoPurchase;
 import enums.LottoRank;
 import utils.LottoNumberFormatter;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class LottoView {
 
     final Scanner scanner = new Scanner(System.in);
 
-    public int inputMoneyView() {
+    public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요");
-        int money = scanner.nextInt();
-        return money;
+        return scanner.nextInt();
+    }
+
+    public int inputManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextInt();
     }
 
     public void printLottosView(List<Lotto> lottos) {
@@ -26,16 +30,13 @@ public class LottoView {
     }
 
     public List<Integer> inputWinningNumbersView() {
-        scanner.nextLine();
         System.out.println();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        String numbersString = scanner.nextLine();
-        List<Integer> inputNumbers = LottoNumberFormatter.parse(numbersString);
-        return inputNumbers;
+        return inputLotto();
     }
 
     public void printLotto(Lotto lotto) {
-        List<LottoNumber> lottoNumbers = lotto.getLottoNumbers();
+        Set<LottoNumber> lottoNumbers = lotto.getLottoNumbers();
         System.out.println(lottoNumbers);
 
     }
@@ -54,6 +55,12 @@ public class LottoView {
     }
 
     private void printSingleRankResult(Map<LottoRank, Integer> resultByRank, LottoRank lottoRank) {
+
+        if (lottoRank == LottoRank.MATCH_5_BONUS) {
+            System.out.println(lottoRank.getMatchCount() + "개 일치, 보너스 볼 일치 (" + lottoRank.getPrize() + ") - " + resultByRank.get(lottoRank) + "개");
+            return;
+        }
+
         if (!(lottoRank == LottoRank.NO_MATCH)) {
             System.out.println(lottoRank.getMatchCount() + "개 일치 (" + lottoRank.getPrize() + ")- " + resultByRank.get(lottoRank));
         }
@@ -61,5 +68,33 @@ public class LottoView {
 
     public void printProfitRate(Double profitRate) {
         System.out.println("총 수익률은 " + profitRate + "입니다.");
+    }
+
+    public List<List<Integer>> inputManualLottos(LottoCount lottoCount) {
+        scanner.nextLine();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<List<Integer>> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount.getLottoCount(); i++) {
+            List<Integer> inputLotto = inputLotto();
+            lottos.add(inputLotto);
+        }
+        return lottos;
+    }
+
+    private List<Integer> inputLotto() {
+        String numbersString = scanner.nextLine();
+        return LottoNumberFormatter.parse(numbersString);
+    }
+
+    public void printPurchaseInfo(LottoPurchase lottoPurchase) {
+        int autoLottoCount = lottoPurchase.getAutoCount().getLottoCount();
+        int manualLottoCount = lottoPurchase.getManualCount().getLottoCount();
+        System.out.println("수동으로 " + manualLottoCount + ", 자동으로 " + autoLottoCount + "를 구매했습니다.");
+        printLottosView(lottoPurchase.getLottos());
+    }
+
+    public int inputBonusNumberView() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return scanner.nextInt();
     }
 }
