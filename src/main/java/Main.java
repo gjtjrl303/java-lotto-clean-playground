19f3590e0c5fae08617ece68387d29ee2529d081
gjtjrl303 <gjtjrl303@naver.com>
2025-04-02 20:18:41 +0900

    refactor: Main에서 run을 실행후 컨트롤러에서 로또 프로그램의 흐름을 제어하도록 변경

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import controller.LottoController;
+import domain.Lotto;
 import domain.LottoMachine;
+import domain.LottoNumber;
 import domain.numberGenerator.LottoNumberGenerator;
 import domain.numberGenerator.NumberGenerator;
 import domain.LottoShop;
 import view.LottoView;
+
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -15,13 +20,7 @@ public class Main {
 
         LottoController lottoController = new LottoController(lottoShop, new LottoView());
 
-        lottoController.inputMoney();
-        lottoController.printLottoCount();
-        lottoController.createLottos();
-        lottoController.printLottos();
-        lottoController.inputWinningNumbers();
-        lottoController.printResultByRank();
-        lottoController.printProfitRate();
+        lottoController.run();
     }
 
 }
