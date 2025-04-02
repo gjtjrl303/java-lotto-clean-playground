8a36927b08e9e90d43ec3c9ad67fb60a7a4fc65c
gjtjrl303 <gjtjrl303@naver.com>
2025-04-02 20:24:08 +0900

    refactor: LottoMachine이 수를 반환하는 역할만 하도록 수정

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -2,34 +2,26 @@ package domain;
 
 import domain.numberGenerator.NumberGenerator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoMachine {
 
     private final NumberGenerator numberGenerator;
-    private final Lottos lottos;
-    private Money money;
 
 
     public LottoMachine(NumberGenerator numberGenerator) {
-        lottos = new Lottos();
         this.numberGenerator = numberGenerator;
     }
 
-    public void inputMoney(Money money) {
-        this.money = money;
-    }
-
-    public void generateLottos() {
-        for (int i = 0; i < money.getPurchasedLottoCount(); i++) {
+    public List<Lotto> generateLottos(LottoCount lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount.getLottoCount(); i++) {
             Lotto lotto = createSingleLotto();
-            saveLotto(lotto);
+            lottos.add(lotto);
         }
-    }
-
-    public List<Lotto> getLottos() {
-        return lottos.getLottos();
+        return lottos;
     }
 
     private Lotto createSingleLotto() {
@@ -37,13 +29,4 @@ public class LottoMachine {
         Collections.sort(numbers);
         return Lotto.from(numbers);
     }
-
-    private void saveLotto(Lotto lotto) {
-        lottos.addLotto(lotto);
-    }
-
-    public int getLottoCount() {
-        return (int) money.getPurchasedLottoCount();
-    }
-
 }
